65e5e12b1120e3a0c3af15b09584ad46646ee900
tiger <thisisstreamliner@gmail.com>
2024-03-18 23:32:28 +0200

    feat: mapping arrays to elements

--- src/App.js
+++ src/App.js
@@ -8,22 +8,28 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      name: 'Tiger'
+      monsters: [
+        {
+          name: 'Frankenstein',
+        },
+        {
+          name: 'Dracula',
+        },
+        {
+          name: 'Zombie',
+        }
+      ]
     }
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name}
-          </p>
-          <button>
-            Change Name  
-          </button>
-        </header>
+        {
+          this.state.monsters.map((monster) => {
+            return <h1>{monster.name}</h1>
+          })
+        }
       </div>
     );
   }
