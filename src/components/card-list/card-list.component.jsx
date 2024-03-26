b7edb5adc2312a30f62526c21a4b256f2f299d26
tiger <thisisstreamliner@gmail.com>
2024-03-26 18:22:18 +0200

    fix: card-list

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,7 +3,10 @@ import { Component } from "react";
 class CardList extends Component {
 
     render() {
-        const {monsters} = this.props.monsters;
+        const {monsters} = this.props;
+        if (monsters.length === 0) {
+            return <h1>Loading...</h1>
+        }
         return (
             monsters.map((monster) => {
               return <div key={monster.id}><h1>{monster.name}</h1></div>
