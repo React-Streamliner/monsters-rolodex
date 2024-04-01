17b0acf4ac923fdb42b2febd2c5a261f32a5d0f2
tiger <thisisstreamliner@gmail.com>
2024-04-01 17:52:09 +0300

    feat: add title. Add new font

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends Component {
     });
     return (
       <div className="App">
+        <h1 className='app-title'>Monsters Rolodex</h1>
         <SearchBox
           className='monster-search-box'
           placeholder='search monsters' 
