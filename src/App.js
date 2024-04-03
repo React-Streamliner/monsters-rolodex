1be23e469dc405196fc8eaf07505eaf4ca19bee3
tiger <thisisstreamliner@gmail.com>
2024-04-03 14:25:57 +0300

    refactor: useEffect to filter and fetch monsters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 
 const App = () => {
   const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLowerCase();
@@ -19,8 +34,10 @@ const App = () => {
         placeholder='search monsters' 
         onChangeHandler={onSearchChange}
       />
-      <CardList monsters={[]}/>
+      <CardList monsters={filteredMonsters}/>
     </div>
   );
 
 }
+
+export default App;
